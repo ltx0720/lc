6890cf48c1138740bfaf59044c85d88fb2e2d2bc
ltx <ltx0720@gmail.com>
2022-05-05 12:12:48 +0800

    week04

--- src/week04/设计跳表.java
+++ src/week04/设计跳表.java
@@ -8,7 +8,7 @@ class 设计跳表 {
 
     private final static int MAX_LEVEL = 32;
 
-    int currentLevel = 0;
+    private int currentLevel = 0;
 
     public 设计跳表() {
         head = new Node(MAX_LEVEL);
@@ -22,11 +22,11 @@ class 设计跳表 {
         int level = randomLevel();
         Node newNode = new Node(num, level);
         Node preNode = findPreNode(num, level);
-        for (int i=currentLevel; i>=0; i--) {
+        for (int i = currentLevel; i >= 0; i--) {
             if (i > level) {
                 continue;
             }
-            // 下一层只需要从上一层的位置往左找到 val > target 的前一个节点
+            // 从上一层的位置往右找到 val > target 的前一个节点
             while (preNode.next[i] != null && num > preNode.next[i].val) {
                 preNode = preNode.next[i];
             }
@@ -34,8 +34,9 @@ class 设计跳表 {
             preNode.next[i] = newNode;
         }
 
-        if (level > currentLevel){
-            for (int i = currentLevel+1; i <= level; i++) {
+        // 层数大于当前层数, 直接从head指向该node
+        if (level > currentLevel) {
+            for (int i = currentLevel + 1; i <= level; i++) {
                 head.next[i] = newNode;
             }
             currentLevel = level;
@@ -47,8 +48,9 @@ class 设计跳表 {
         if (preNodeInfo == null) {
             return false;
         }
-        Node preNode= preNodeInfo.node;
-        for (int i=preNodeInfo.level; i>=0; i--) {
+        Node preNode = preNodeInfo.node;
+        for (int i = preNodeInfo.level; i >= 0; i--) {
+            // 后面没有节点, 直接处理下一层
             if (preNode.next[i] == null) {
                 continue;
             }
@@ -56,7 +58,7 @@ class 设计跳表 {
             while (num > preNode.next[i].val) {
                 preNode = preNode.next[i];
             }
-            preNode.next[i] =  preNode.next[i].next[i];
+            preNode.next[i] = preNode.next[i].next[i];
         }
         return true;
     }
@@ -70,6 +72,9 @@ class 设计跳表 {
         return level;
     }
 
+    /**
+     * 在指定level层查找target前一个节点
+     */
     private Node findPreNode(int target, int level) {
         Node node = head;
         while (node.next[level] != null && target > node.next[level].val) {
@@ -78,6 +83,9 @@ class 设计跳表 {
         return node;
     }
 
+    /**
+     * 在所有的层查找target前一个节点
+     */
     private NodeInfo findPreNode(int target) {
         Node node = head;
         int level = currentLevel;
@@ -86,14 +94,17 @@ class 设计跳表 {
                 node = node.next[level];
             }
             if (node.next[level] != null && node.next[level].val == target) {
-                NodeInfo nodeInfo = new NodeInfo(node, level);
-                return nodeInfo;
+                // 连同所在层级一块返回
+                return new NodeInfo(node, level);
             }
             level--;
         }
         return null;
     }
 
+    /**
+     * 在指定所有的层查找target是否存在
+     */
     private Node findNode(int target) {
         Node node = head;
         int level = MAX_LEVEL;
@@ -115,34 +126,29 @@ class 设计跳表 {
 //        skiplist.add(1);
 //        skiplist.add(2);
 //        skiplist.add(3);
-//        boolean search = skiplist.search(0);// 返回 false
+//        boolean search = skiplist.search(0);
 //        skiplist.add(4);
 //        boolean search1 = skiplist.search(1);
 //        skiplist.add(5);
 //        boolean search2 = skiplist.search(3);
-//        boolean search3 = skiplist.search(6);// 返回 false，1 已被擦除
+//        boolean search3 = skiplist.search(6);
 //
 //        System.out.println(search);
 
-        while (true) {
-            设计跳表 skiplist = new 设计跳表();
-            skiplist.add(9);
-            skiplist.add(4);
-            skiplist.add(5);
-            skiplist.add(6);
-            skiplist.add(9);
-            boolean erase = skiplist.erase(2);
-            boolean erase1 = skiplist.erase(1);
-            skiplist.add(2);
-            skiplist.search(7);
-            skiplist.search(4);
-            skiplist.add(5);
-            boolean erase2 = skiplist.erase(6);
-        }
-
-
+        设计跳表 skiplist = new 设计跳表();
+        skiplist.add(9);
+        skiplist.add(4);
+        skiplist.add(5);
+        skiplist.add(6);
+        skiplist.add(9);
+        boolean erase = skiplist.erase(2);
+        boolean erase1 = skiplist.erase(1);
+        skiplist.add(2);
+        skiplist.search(7);
+        skiplist.search(4);
+        skiplist.add(5);
+        boolean erase2 = skiplist.erase(6);
     }
-
 
 }
 
@@ -152,12 +158,12 @@ class Node {
 
     public Node(int val, int level) {
         this.val = val;
-        this.next = new Node[level+1];
+        this.next = new Node[level + 1];
     }
 
     public Node(int level) {
         this.val = -1;
-        this.next = new Node[level+1];
+        this.next = new Node[level + 1];
     }
 }
 
